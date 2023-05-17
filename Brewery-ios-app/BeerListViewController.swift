9508aad3c93628d9429fb8941cadeba749214670
jinan <ja.choi@sweetk.co.kr>
2023-05-17 20:23:25 +0900

    1. snapKit, KingFisher 추가 2. sceneDelegate 수정 3. BeerListViewController 테이블뷰 컨트롤러 추가

---
+++ Brewery-ios-app/BeerListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BeerListViewController.swift
+//  Brewery-ios-app
+//
+//  Created by 최진안 on 2023/05/17.
+//
+
+import UIKit
+
+class BeerListViewController: UITableViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    
+    
+    
+}
