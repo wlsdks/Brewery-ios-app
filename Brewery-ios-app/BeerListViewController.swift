5f1b7130e4417b6f83b5764267e76385c73e723e
jinan <ja.choi@sweetk.co.kr>
2023-05-17 21:02:15 +0900

    1. 테이블 뷰 셀에 코드추가

--- Brewery-ios-app/BeerListViewController.swift
+++ Brewery-ios-app/BeerListViewController.swift
@@ -19,6 +19,10 @@ class BeerListViewController: UITableViewController {
         //UINavigationBar설정
         title = "브루어리 앱"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //UITableView 설정
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
+        tableView.rowHeight = 150 //높이설정
     }
     
     
@@ -30,6 +34,17 @@ extension BeerListViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
+        
+        
+        // 맥주값을 가져와서 configure에 넣어준다.
+        let beer = beerList[indexPath.row]
+        cell.configure(with: beer)
+        
+        return cell
     }
     
     
