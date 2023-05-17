b8bca832a02b793769e2923a66037929107c430a
jinan <ja.choi@sweetk.co.kr>
2023-05-17 20:17:50 +0900

    Initial Commit

---
+++ Brewery-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brewery-ios-app
+//
+//  Created by 최진안 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
