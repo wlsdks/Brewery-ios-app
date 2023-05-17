e7fc8f135220d70daa7f18dbc4a58f53523e2580
jinan <ja.choi@sweetk.co.kr>
2023-05-17 22:20:06 +0900

    1. 데이터를 25개씩만 목록을 로드하도록 코드 수정

--- Brewery-ios-app/BeerListViewController.swift
+++ Brewery-ios-app/BeerListViewController.swift
@@ -11,6 +11,7 @@ class BeerListViewController: UITableViewController {
 
     
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
     var currentPage = 1
     
     
@@ -24,6 +25,7 @@ class BeerListViewController: UITableViewController {
         //UITableView 설정
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150 //높이설정
+        tableView.prefetchDataSource = self
         
         fetchBeer(of: currentPage)
     }
@@ -33,7 +35,7 @@ class BeerListViewController: UITableViewController {
 }
 
 // UITableView DataSource, Delegate
-extension BeerListViewController {
+extension BeerListViewController: UITableViewDataSourcePrefetching {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
@@ -41,7 +43,6 @@ extension BeerListViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
-        
         
         // 맥주값을 가져와서 configure에 넣어준다.
         let beer = beerList[indexPath.row]
@@ -57,12 +58,25 @@ extension BeerListViewController {
         detailViewController.beer = selectedBeer
         self.show(detailViewController, sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard currentPage != 1 else { return }
+        
+        indexPaths.forEach {
+            //25개의 데이터가 불려온 다음 다음 페이지의 데이터 목록을 불러온다.
+            if ($0.row + 1)/25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
 }
 
 //Data Fetching
 private extension BeerListViewController {
+
     func fetchBeer(of page: Int) {
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+              dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -104,5 +118,6 @@ private extension BeerListViewController {
             }
         }
         dataTask.resume() //반드시 resume을 해줘야 한다.
+        dataTasks.append(dataTask)
     }
 }
